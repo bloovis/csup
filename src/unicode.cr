8d935b4ae7cd200ced91a575ebcbb080790eac4c
Mark Alexander <marka@pobox.com>
2023-10-26 19:23:18 -0400

    Move test code to test.cr.

--- src/unicode.cr
+++ src/unicode.cr
@@ -4,6 +4,7 @@ lib LibC
   alias WChar = UInt32
 
   fun wcswidth(s : WChar*, n : SizeT) : Int
+  fun wcwidth(c : WChar) : Int
 end
 
 module Unicode
@@ -11,26 +12,17 @@ module Unicode
 
   # cjk is ignored, but present for Ruby compatibility
   def width(s : String, cjk : Bool = false) : Int32
-    #puts "Determining width of #{s}"
     width = 0
     chreader = Char::Reader.new(s)
     chreader.each do |ch|
-      #puts "ch = #{ch}(#{ch.ord})"
       wc : LibC::WChar = ch.ord.to_u
-      wclen = LibC.wcswidth(pointerof(wc), 1)
+      wclen = LibC.wcwidth(wc)
       if wclen < 0
 	wclen = 1
       end
-      #puts "wclen = #{wclen}"
       width += wclen
     end
     width
   end
 
 end
-
-LibNCurses.setlocale(0, "")
-s = "this is a test"
-puts "display width of '#{s}' = #{Unicode.width(s)}"
-s = "你好"
-puts "display width of '#{s}' = #{Unicode.width(s)}"
