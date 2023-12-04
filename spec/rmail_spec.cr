da8ecd94e5de767c771b5e236a99e909e1dd6989
Mark Alexander <marka@pobox.com>
2023-12-04 05:49:11 -0500

    Add quoted-printable test.

--- spec/rmail_spec.cr
+++ spec/rmail_spec.cr
@@ -1,5 +1,6 @@
 require "./spec_helper"
-require "../src/rmail/address.cr"
+require "../src/rmail/address"
+require "../src/rmail/quoted_printable"
 
 describe RMail::Address do
   it "parses addresses in a group" do
@@ -45,3 +46,35 @@ describe RMail::Address do
   end
 end
 
+describe RMail::QuotedPrintable do
+  it "encodes and decodes a quotable-printable text" do
+    data = <<-HTM
+<html lang=\"ja\">\r
+<head>\r
+  <title>日本語タイトル</title>\r
+</head>\r
+<body>\r
+  <h1>見出し</h1>\r
+  <p>本文<p>\r
+</body>\r
+</html>\r
+HTM
+    encoded = RMail::QuotedPrintable.encode(data)
+    expected = <<-HTM
+<html lang=3D"ja">\r
+<head>\r
+  <title>=E6=97=A5=E6=9C=AC=E8=AA=9E=E3=82=BF=E3=82=A4=E3=83=88=E3=83=AB</t=\r
+itle>\r
+</head>\r
+<body>\r
+  <h1>=E8=A6=8B=E5=87=BA=E3=81=97</h1>\r
+  <p>=E6=9C=AC=E6=96=87<p>\r
+</body>\r
+</html>=0D
+HTM
+    encoded.should eq(expected)
+
+    decoded = RMail::QuotedPrintable.decode_string(encoded)
+    decoded.should eq(data)
+  end
+end
