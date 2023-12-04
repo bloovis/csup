09c7831df2c2392644c87da260ccdfce55688da7
Mark Alexander <marka@pobox.com>
2023-12-04 06:31:55 -0500

    Add tests for RMail::Utils module.

--- spec/rmail_spec.cr
+++ spec/rmail_spec.cr
@@ -1,6 +1,6 @@
 require "./spec_helper"
 require "../src/rmail/address"
-require "../src/rmail/quoted_printable"
+require "../src/rmail/utils"
 
 describe RMail::Address do
   it "parses addresses in a group" do
@@ -78,3 +78,53 @@ HTM
     decoded.should eq(data)
   end
 end
+
+describe RMail::Utils do
+  it "unquotes a string" do
+    s = "\"this is a string\""
+    unquoted = RMail::Utils.unquote(s)
+    unquoted.should eq("this is a string")
+
+    s = "\"this is a string\\\""
+    unquoted = RMail::Utils.unquote(s)
+    unquoted.should eq(s)
+  end
+
+  it "decodes base64 data" do
+    s = "U2VuZCByZWluZm9yY2VtZW50cw==\n"
+    decoded = RMail::Utils.base64_decode(s)
+    decoded.should eq("Send reinforcements")
+  end
+
+  it "decodes quoted-printable text" do
+    encoded = <<-HTM
+<html lang=3D"ja">\r
+<head>\r
+  <title>=E6=97=A5=E6=9C=AC=E8=AA=9E=E3=82=BF=E3=82=A4=E3=83=88=E3=83=AB</t=\r
+itle>\r
+</head>\r
+<body>\r
+  <h1>=E8=A6=8B=E5=87=BA=E3=81=97</h1>\r
+  <p>=E6=9C=AC=E6=96=87<p>\r
+</body>\r
+</html>=0D
+HTM
+
+    expected = <<-HTM
+<html lang=\"ja\">\r
+<head>\r
+  <title>日本語タイトル</title>\r
+</head>\r
+<body>\r
+  <h1>見出し</h1>\r
+  <p>本文<p>\r
+</body>\r
+</html>\r
+HTM
+
+    decoded = RMail::Utils.quoted_printable_decode(encoded)
+    decoded.should eq(expected)
+  end
+
+end
+
