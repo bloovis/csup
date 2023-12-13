b1047da81dc2ffcfd0f6f5fe07abb479898f9dd7
Mark Alexander <marka@pobox.com>
2023-12-13 12:22:48 -0500

    Add remaining functions to Buffer, though they probably don't work right now.

--- src/buffer.cr
+++ src/buffer.cr
@@ -3,6 +3,7 @@
 
 class Buffer
   alias BufferOpts = Hash(Symbol, Bool | String)
+  alias ColorOpts = Hash(Symbol, Symbol)
 
   getter x : Int32
   getter y : Int32
@@ -12,8 +13,8 @@ class Buffer
   getter atime : Time
   getter mode : Mode | Nil
   getter w : Ncurses::Window
-  getter force_to_top : Bool
-  getter hidden : Bool
+  property force_to_top : Bool
+  property hidden : Bool
   getter system : Bool
 
   def string_opt(opts : BufferOpts, key : Symbol) : String
@@ -48,28 +49,81 @@ class Buffer
     @mode = mode
     @dirty = true
     @focus = false
-    
-    #if opts.has_key?(:title)
-    #  @title = opts[:title].as(String)
-    #else
-    #  @title = ""
-    #end
     @title = string_opt(opts, :title)
-    #if opts.has_key?(:force_to_top)
-    #  @force_to_top = opts[:force_to_top].as(Bool)
-    #else
-    #  @force_to_top = false
-    #end
     @force_to_top = bool_opt(opts, :force_to_top)
-    #if opts.has_key?(:hidden)
-    #  @hidden = opts[:hidden].as(Bool)
-    #else
-    #  @hidden = false
-    #end
     @hidden = bool_opt(opts, :hidden)
     @x, @y, @width, @height = 0, 0, width, height
     @atime = Time.unix 0
     @system = bool_opt(opts, :system)
+  end
+
+  def content_height; @height - 1; end
+  def content_width; @width; end
+
+  def resize(rows, cols)
+    return if cols == @width && rows == @height
+    @width = cols
+    @height = rows
+    @dirty = true
+    mode.resize rows, cols
+  end
+
+  def redraw(status)
+    if @dirty
+      draw status
+    else
+      draw_status status
+    end
+
+    commit
+  end
+
+  def mark_dirty; @dirty = true; end
+
+  def commit
+    @dirty = false
+    @w.noutrefresh
+  end
+
+  def draw(status)
+    @mode.draw
+    draw_status status
+    commit
+    @atime = Time.now
+  end
+
+  ## s nil means a blank line!
+  def write(y, x, s, opts : ColorOpts)
+    return if x >= @width || y >= @height
+
+    @w.attrset Colormap.color_for(opts[:color] || :none, opts[:highlight])
+    s ||= ""
+    maxl = @width - x # maximum display width width
+
+    # fill up the line with blanks to overwrite old screen contents
+    @w.mvaddstr y, x, " " * maxl unless opts[:no_fill]
+
+    @w.mvaddstr y, x, s.slice_by_display_length(maxl)
+  end
+
+  def clear
+    @w.clear
+  end
+
+  def draw_status(status)
+    write @height - 1, 0, status, {:color => :status_color}
+  end
+
+  def focus
+    @focus = true
+    @dirty = true
+    @mode.focus
+  end
+
+  def blur
+    @focus = false
+    @dirty = true
+    @mode.blur
   end
 end
 
